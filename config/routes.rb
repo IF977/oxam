edbe39f3a55f15e629458a9dc9394a2f785a32ab
ygoleite <ygolmelo@gmail.com>
2015-06-29 16:16:30 +0000

    Commit da view e de algumas funcionalidades

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
+Myapp::Application.routes.draw do
+  get "home/index"
+  get "home/minor"
+  
+
   resources :quests
   resources :quests
   resources :admins
@@ -9,8 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+root to: 'home#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
